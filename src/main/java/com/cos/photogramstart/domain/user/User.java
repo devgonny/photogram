308d3f6146cac0f8ca617651c97bc40503fa63ce
devgonny <dgryu92@gmail.com>
2023-01-02 11:35:46 +0900

    valid Check

--- src/main/java/com/cos/photogramstart/domain/user/User.java
+++ src/main/java/com/cos/photogramstart/domain/user/User.java
@@ -1,6 +1,8 @@
 package com.cos.photogramstart.domain.user;
 
 import java.time.LocalDateTime;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -21,17 +23,30 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id // Primary Key를 지정해주는 어노테이션
     private Integer id; // 데이터가 들어갈 때 마다 번호를 매겨줄것임.
+    @Column(unique=true, length = 20, nullable = false)
     private String username; // 아이디
+    @Column(nullable = false)
     private String password; // 비밀번호
+    @Column(nullable = false)
     private String name; // 별명
+    
     private String website; // 개인 웹사이트 주소
+    
     private String bio; // 자기소개
+    
+    @Column(nullable = false)
     private String email; // 이메일
+    
     private String phone; // 전화번호
+    
     private String gender; // 성별
+    
     private String prifileImageUrl; // 프로필 사진
+    
     private String role; // 권한 
+    
     private LocalDateTime createDate; // 데이터가 입력된 시간.
+    
     @PrePersist
     public void createDate() {
         this.createDate = LocalDateTime.now();
--- src/main/java/com/cos/photogramstart/web/AuthController.java
+++ src/main/java/com/cos/photogramstart/web/AuthController.java
@@ -1,14 +1,22 @@
 package com.cos.photogramstart.web;
 
-import com.cos.photogramstart.domain.user.User;
-import com.cos.photogramstart.service.AuthService;
-import com.cos.photogramstart.web.dto.auth.SignupDto;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.validation.Valid;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import com.cos.photogramstart.domain.user.User;
+import com.cos.photogramstart.service.AuthService;
+import com.cos.photogramstart.web.dto.auth.SignupDto;
+
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor // final이 붙은것에 대한 생성자를 만들어주는 어노테이션
@@ -36,9 +44,16 @@ public class AuthController {
     }
     // 회원가입 기능
     @PostMapping("/auth/signup")
-    public String signup(SignupDto signupDto) {
-        // log.info(signupDto.toString());
-
+    public String signup(@Valid SignupDto signupDto, BindingResult bindingResult) {
+    	
+    	if(bindingResult.hasErrors()) {
+    		Map<String, String> errorMap = new HashMap<>();
+    		
+    		for(FieldError error : bindingResult.getFieldErrors()) {
+    			errorMap.put(error.getField(), error.getDefaultMessage());
+    			System.out.println(error.getDefaultMessage());
+    		}
+    	}
         // User Object에 SignupDto 데이터를 삽입하려고 한다.
         // User 오브젝트에 signupDto에서 방금 만들었던 toEntity 데이터를 넣어주자.
         User user = signupDto.toEntity();
