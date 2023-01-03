3b6f7b1c78591f8a71512401296bd74e269dd9d6
devgonny <dgryu92@gmail.com>
2023-01-03 14:59:27 +0900

    DetailsService 완성하고 로그인 세션 저장 완료

--- src/main/java/com/cos/photogramstart/config/auth/PrincipalDetailsService.java
+++ src/main/java/com/cos/photogramstart/config/auth/PrincipalDetailsService.java
@@ -5,14 +5,31 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.cos.photogramstart.domain.user.User;
+import com.cos.photogramstart.domain.user.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Service
-public class PrincipalDetailsService implements UserDetailsService{
+public class PrincipalDetailsService implements UserDetailsService {
+
+	private final UserRepository userRepository;
+
+	// 1. 스프링시큐리티가 password는 알아서 확인해주기 때문에 신경안써도 된다.
+	// 2. 리턴이 잘 되면 자동으로 UserDetails 타입을 session으로 만들어준다.
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		System.out.println(username + "실행됨??");
-		return null;
+
+		User userEntity = userRepository.findByUsername(username);
+
+		if (userEntity == null) { // username을 찾지 못했다면
+			return null; // null을 리턴하고
+		} else { // username을 찾았다면
+						//userEntity를 리턴할 수 없으므로 PrincipalDetails 생성(UserDetails상속)
+			return new PrincipalDetails(userEntity); // principalDetails를 session에 저장한다.
+		}
 	}
 
-	
 }
--- src/main/java/com/cos/photogramstart/domain/user/UserRepository.java
+++ src/main/java/com/cos/photogramstart/domain/user/UserRepository.java
@@ -2,6 +2,7 @@ package com.cos.photogramstart.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Integer>{
-
+public interface UserRepository extends JpaRepository<User, Integer> {
+	//JPA query method
+	User findByUsername(String username);
 }
